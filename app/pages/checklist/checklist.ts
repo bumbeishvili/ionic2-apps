b89bc85a1bea2844113b531df3204e17e32b5a6b
Davit Bumbeishvili <davit.bumbeishvili@gmail.com>
2016-08-27 20:34:26 +0400

    initial commit

---
+++ app/pages/checklist/checklist.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ChecklistPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/checklist/checklist.html',
+})
+export class ChecklistPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
