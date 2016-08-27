d7d1ae304805ba15a5a80137cdb8ec069dfcef54
Davit Bumbeishvili <davit.bumbeishvili@gmail.com>
2016-08-27 22:29:58 +0400

    quicklist - added checklistmodel

--- quicklist/app/providers/checklistmodel/checklistmodel.ts
+++ quicklist/app/providers/checklistmodel/checklistmodel.ts
@@ -10,8 +10,41 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class Checklistmodel {
+  checklist: any;
+  checklistObserver: any;
 
-  constructor(private http: Http) {}
+  constructor(public title: string, public items: any[]) {
+    this.items = items;
+  }
+
+  addItem(title): void {
+    this.items.push({
+      title: title,
+      checked: false
+    });
+  }
+
+
+  removeItem(item): void {
+    let index = this.items.indexOf(item);
+
+    if (index > -1) this.items.splice(index, 1);
+  }
+
+  renameItem(item, title): void {
+    let index = this.items.indexOf(item);
+    if (index > -1) {
+      this.items[index].title = title;
+    }
+  }
+
+  setTitle(title):void{
+    this.title = title;
+  }
+
+  toggleItem (item) :void{
+    item.checked = !item.checked;
+  }
 
 }
 
